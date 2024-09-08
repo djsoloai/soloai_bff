1bbf8993a47c3fc631d44e27ac39c982f08cb9ca
soloai <djsoloai@gmail.com>
2024-09-09 00:02:23 +0800

    authInvite

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const Koa = require("koa");
 const cors = require("koa2-cors");
 const Router = require("koa-router");
-const { proxy } = require("./proxy")
-const {koaBody} = require("koa-body")
+const { proxy } = require("./proxy");
+const { koaBody } = require("koa-body");
 
 const app = new Koa();
 const router = new Router();
 
 router.get(/proxy\/*/, async (ctx) => {
-  const res = await proxy(ctx)
-  ctx.body = res
+  const res = await proxy(ctx);
+  ctx.body = res;
 });
 
 router.post(/proxy\/*/, koaBody(), async (ctx) => {
-  const res = await proxy(ctx)
-  console.log(res)
-  ctx.body = res
+  const res = await proxy(ctx);
+  ctx.body = res;
+});
+
+router.get("authInvite", async (ctx) => {
+  const { code } = ctx.query;
+  if (code === "A37b5H") ctx.body = { result: true };
+  else ctx.body = { result: false };
 });
 
 app.use(
